6336012f88e9f57bbffa64b516b3a8665ea852b5
Mohamed Mohamed <demahom93@yahoo.co.uk>
2021-03-26 10:48:15 +0300

    whitenoise

--- farm191/settings.py
+++ farm191/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 'django.contrib.sites',  
+    'whitenoise.runserver_nostatic',
     'hr',
     'finance',
     'inventory',
@@ -78,6 +79,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
         # CORS
@@ -164,6 +166,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # configuring the location for media
 MEDIA_URL = '/media/'
